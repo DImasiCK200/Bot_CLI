153b17a6af41c93f59545715eff6d9f750efd2f8
ShpkUshnk <zgfzgfzgaf@gmail.com>
2026-02-21 20:47:28 +0700

    new inventory menu transsition

--- src/assets/menu/account/inventoryMenu.js
+++ src/assets/menu/account/inventoryMenu.js
@@ -4,34 +4,39 @@ import {
   DeleteAccountCommand,
 } from "../../../modules/commands/index.js";
 
-const countBy = (array, param) => {
-  return array.reduce((acc, item) => {
-    const name = item[param];
+export const inventoryMenu = (createCommand) => {
+  return new Menu({
+    title: "Inventory",
+    descriptionFn: async (ctx) => {
+      let text = "";
+      const steamApi = await ctx.accountManager.getSteamAPI();
+      // const { inventory } = await steamApi.getInventory();
+      // const groupedInv = Object.groupBy(
+      //   inventory,
+      //   (item) => item.market_hash_name,
+      // );
 
-    acc[name] = (acc[name] || 0) + 1;
-    return acc;
-  }, {});
+      text += `Account: ${steamApi?.steamUser?.name || ctx.accountManager.accountName || "not selected"}\n\n`;
+      // text += Object.entries(groupedInv)
+      //   .map(([name, items]) => `${name}: ${items.length} `)
+      //   .join("\n");
+      return text;
+    },
+    itemsFn: async (ctx) => {
+      const items = [];
+      if (!createCommand) return items;
+
+      const steamApi = await ctx.accountManager.getSteamAPI();
+      const { inventory } = await steamApi.getInventory();
+      const groupedInv = Object.groupBy(
+        inventory,
+        (item) => item.market_hash_name,
+      );
+      Object.entries(groupedInv).map(([name, itemArray]) => {
+        items.push(new MenuItem(name, createCommand({ name, itemArray })));
+      });
+
+      return items;
+    },
+  });
 };
-
-export const inventoryMenu = new Menu({
-  title: "Inventory",
-  descriptionFn: async (ctx) => {
-    let text = "";
-    const steamApi = await ctx.accountManager.getSteamAPI();
-    const { inventory } = await steamApi.getInventory();
-    const countedInv = countBy(inventory, "market_hash_name");
-
-    text += `Account: ${steamApi.steamUser.name ?? "not selected"}\n\n`;
-    text += Object.keys(countedInv)
-      .map((key) => {
-        return `${key}: ${countedInv[key]}`;
-      })
-      .join("\n");
-    return text;
-  },
-  itemsFn: (ctx) => {
-    const items = [];
-
-    return items;
-  },
-});
